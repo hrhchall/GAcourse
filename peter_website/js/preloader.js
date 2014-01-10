84b928a6c915cc5def949cec3fb660c865b3cf6e
hrhchall <holly.challenger@gmail.com>
2014-01-10 15:14:27 +0000

    peter_morechanges

--- peter_website/js/preloader.js
+++ peter_website/js/preloader.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $("body").queryLoader2({
-        barColor: "#6e6d73",
-        backgroundColor: "#fff1b0",
-        percentage: true,
-        barHeight: 1,
-        completeAnimation: "grow",
-        minimumTime: 100
-    });
+    $("body").queryLoader2();
+       
 });
