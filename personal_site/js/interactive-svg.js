76ea94a4728a177daed91d75588f646a202e1132
hrhchall <holly.challenger@gmail.com>
2014-02-07 12:23:14 +0000

    fallback_svg

--- personal_site/js/interactive-svg.js
+++ personal_site/js/interactive-svg.js
@@ -1,12 +1,16 @@
+ if($(window).width() > 870) {
 $(function(){
 
 	$("#stage").load('svg_russiandoll_interactive.svg',function(response){
 
 		$(this).addClass("svgLoaded");
 		
-		if(!response){ // Error loading SVG
-			$(this).html('Error loading SVG. Be sure you are running from a the http protocol (not locally) and have read <strong><a href="http://tympanus.net/codrops/?p=13831#the-javascript">this important part of the tutorial</a></strong>');
-		}
-
-	});
+		
+			});
 });
+}
+		 else {
+    $(".fallback").load();
+    console.log("fallback loaded")
+}
+
